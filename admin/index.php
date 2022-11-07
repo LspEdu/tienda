1158101b040157f1de37eee00aa07fa394432cf9
edu <edu@PortatilDebian.home>
2022-11-07 18:12:59 +0100

    Creación de admin/index.php

--- admin/index.php
+++ admin/index.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Portal</title>
+    <title>Listado de Artículos</title>
 </head>
 <body>
-    TODO
+    Listado de artículos
 </body>
 </html>
